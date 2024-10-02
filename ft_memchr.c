a8876b4391e162c010cabcf4381970a87d699aa6
Damian Galeano <dj.damian.b@hotmail.com>
2024-10-02 22:49:45 +0200

    add memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 22:23:00 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/02 22:23:00 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memchr(const void *ptr, int value, size_t num)
+{
+	size_t			i;
+	unsigned char	*p;
+
+	p = (unsigned char *) ptr;
+	i = 0;
+	while (i < num)
+	{
+		if (p[i] == (unsigned char ) value)
+			return ((void *)&p[i]);
+		i++;
+	}
+	return (NULL);
+}
